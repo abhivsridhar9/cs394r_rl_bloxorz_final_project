8b04d378013c0abf377f0daa8209f290a838b00c
abhivsridhar9 <48297435+abhivsridhar9@users.noreply.github.com>
2024-04-06 09:59:11 -0500

    more progress to rotation logic

--- block.py
+++ block.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Block:
 
-    def __init__(self, xr, c1, r2, c2):
+    def __init__(self, r1, c1, r2, c2):
         self._r1 = r1
         self._r2 = r2
         self._c1 = c1
@@ -15,32 +15,31 @@ class Block:
         self._c1 = c1
         self._c2 = c2
 
+    def set_coords(self, r1, c1, r2, c2):
+        self._r1 = r1
+        self._r2 = r2
+        self._c1 = c1
+        self._c2 = c2
+        
     def get_coords(self):
         return np.ndarray([self._r1, self._r2, self._c1, self._c2])
 
     def isUpright(self):
-        if self._r1 == self._c1 and self._r2 == self._c2:
-            return True
-
-        return False
+        return self._r1 == self._r2 and self._c1 == self._c2
 
     def isWide(self):
-        if self.isUpright:
-            return False
-
-        if self._r2 != self._c2 and self._r1 == self._c1:
-            return True
+        return self._r1 == self._r2 and self._c1 != self._c2
 
         return False
 
     def moveUp(self):
         # vertical
-        if self.isUpright:
+        if self.isUpright():
             self._r1 += 2
             self._c1 += 1
 
         # flat and wide
-        elif self.isWide:
+        elif self.isWide():
             self._c1 += 1
             self._r1 += 1
 
@@ -51,12 +50,12 @@ class Block:
 
     def moveDown(self):
         # vertical
-        if self.isUpright:
+        if self.isUpright():
             self._r1 -= 1
             self._c1 -= 2
 
         # flat and wide
-        elif self.isWide:
+        elif self.isWide():
             self._r1 -= 1
             self._c1 -= 1
 
@@ -68,7 +67,7 @@ class Block:
     # edited
     def moveRight(self):
         # vertical
-        if self.isUpright:
+        if self.isUpright():
             self._r2 += 2
             self._c2 += 1
 
@@ -85,12 +84,12 @@ class Block:
     # edited
     def moveLeft(self):
         # vertical
-        if self.isUpright:
+        if self.isUpright():
             self._r2 -= 2
             self._c2 -= 1
 
         # flat and wide
-        elif self.isWide:
+        elif self.isWide():
             self._r2 -= 1
             self._c2 -= 2
 
