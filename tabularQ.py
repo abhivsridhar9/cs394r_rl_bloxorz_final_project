fa5b7711545ae3aada8a3e9810b21c5599c05313
William Avery <williamaavery4@gmail.com>
2024-04-08 14:36:22 -0500

    code cleanup

--- tabularQ.py
+++ tabularQ.py
@@ -2,40 +2,39 @@ import argparse
 import numpy as np
 from Levels.levelone import LevelOne
 
-ALPHA = 0.1
-GAMMA = 1
 
 def parse():
     parser = argparse.ArgumentParser(description='Tabular Q-Learning')
     parser.add_argument('--num_episodes', default=1000)
-    parser.add_argument("--level", default=1, type=int)
+    parser.add_argument('--level', default=1, type=int)
+    parser.add_argument('--gamma', default=1)
+    parser.add_argument('--alpha', default=0.1)
     args = parser.parse_args()
     return args
 
 def eps_greedy_action_select(Q, s, eps=1e-4):
+    '''
+    Chooses an action following the epsilon greedy policy.
+    Inputs:
+        - Q: the state-action value function to select from
+        - s: the current of the agent
+        - eps: how often to choose a random action uniformly
+    Outputs:
+
+    '''
     if np.random.uniform() < eps:
-        return np.random.randint(0, 3)
+        return -1, np.random.randint(0, 3)
     else:
         max_Q = np.float64("-inf")
-        max_Q_i = 0
+        a = 0
         for i in range(0, 4):
             if s not in Q[i]:
                 Q[i][s] = 0     # instantiate this state, action pair if it does not exist
             Q_i = Q[i][s]
             if Q_i > max_Q:
                 max_Q = Q_i
-                max_Q_i = i
-        return max_Q_i
-    
-def greedy_Q(Q, s):
-    max_Q = np.float64("-inf")
-    for i in range(0, 4):
-        if s not in Q[i]:
-            Q[i][s] = 0        # instantiate this state, action pair if it does not exist
-        Q_i = Q[i][s]
-        if Q_i > max_Q:
-            max_Q = Q_i
-    return max_Q
+                a = i
+        return max_Q, a
 
 
 if __name__ == '__main__':
@@ -54,10 +53,10 @@ if __name__ == '__main__':
         s, done = env.reset(), False
 
         while not done:
-            a = eps_greedy_action_select(Q, s)
+            _, a = eps_greedy_action_select(Q, s)
             s_prime, r, done = env.step(a)
-            max_Q_s_prime_a = greedy_Q(Q, s_prime)
-            Q[a][s] = Q[a][s] + ALPHA * (r + GAMMA * max_Q_s_prime_a - Q[a][s])
+            Q_prime, _ = eps_greedy_action_select(Q, s_prime, eps=0)
+            Q[a][s] = Q[a][s] + args.alpha * (r + args.gamma * Q_prime - Q[a][s])
             s = s_prime
 
     # Final Route
@@ -72,7 +71,7 @@ if __name__ == '__main__':
 
     r_total = 0
     while not done:
-        a = eps_greedy_action_select(Q, s, 0)
+        _, a = eps_greedy_action_select(Q, s, 0)
         s, r, done = env.step(a)
         r_total += r
         print(f'Action: {act_to_lang[a]} | Done: {done} | Reward: {r_total}')
